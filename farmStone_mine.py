47ae616e0776c763be2b083cb22f7c2a18bf532f
Krawstr <149870885+Krawstr@users.noreply.github.com>
2023-11-04 09:58:06 -0300

    Update farmStone_mine.py

--- farmStone_mine.py
+++ farmStone_mine.py
@@ -1,13 +1,19 @@
 import pyautogui as gui
 from time import sleep
 
+blocks = 0
+
 sleep(3)
 while True:
     try:
-        img = gui.locateCenterOnScreen('capturar.png', confidence=0.7)
+        img = gui.locateCenterOnScreen('capturar.png', confidence=0.8)
         gui.mouseDown(img.x, img.y)
-        sleep(0.3)
+        sleep(1)
         gui.mouseUp()
+        print('destry block')
+        blocks += 1
+        print(f'destroyed blocks {blocks}')
     except:
         sleep(0.2)
         print('where is the stone?')
+   
