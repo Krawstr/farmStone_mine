bcd8f2de63d962821e4939c826d49004f1c2301e
Krawstr <149870885+Krawstr@users.noreply.github.com>
2023-11-04 22:02:07 -0300

    farmStone_mine.py

--- farmStone_mine.py
+++ farmStone_mine.py
@@ -6,8 +6,9 @@ img = gui.locateCenterOnScreen('capturar.png', confidence=0.8)
 
 sleep(3)
 while True:
+    img = gui.locateCenterOnScreen('capturar.png', confidence=0.8)
+    
     if img is not None:
-        img = gui.locateCenterOnScreen('capturar.png', confidence=0.8)
         gui.mouseDown(img.x, img.y)
         sleep(1)
         gui.mouseUp()
