2d42c9ca71bd16a3abd5a6ee0bfa4ebbf71dbc77
Krawstr <149870885+Krawstr@users.noreply.github.com>
2023-11-07 20:01:27 -0300

    Atualizar o farmStone_mine.py

--- farmStone_mine.py
+++ farmStone_mine.py
@@ -2,11 +2,11 @@ import pyautogui as gui
 from time import sleep
 
 blocks = 0
-img = gui.locateCenterOnScreen('capturar.png', confidence=0.8)
+img = gui.locateCenterOnScreen('captura.png', confidence=0.8)
 
 sleep(3)
 while True:
-    img = gui.locateCenterOnScreen('capturar.png', confidence=0.8)
+    img = gui.locateCenterOnScreen('captura.png', confidence=0.8)
     
     if img is not None:
         gui.mouseDown(img.x, img.y)
